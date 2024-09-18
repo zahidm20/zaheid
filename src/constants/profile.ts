fc5fdcd01d316460ee9b1892613e0148bdabccd4
zahidm20 <117884005+zahidm20@users.noreply.github.com>
2024-09-18 08:59:54 +0700

    yes

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -5,7 +5,7 @@ export const profileData: Profile = {
   region: "Indonesia",
   CV: "/assets/pdf/cv.pdf",
   divisions: ["Fullstack and Mobile Developer", "Backend Developer", "Mobile Developer", "Software Tester"],
-  pictures: ["/assets/images/micel.png", "/assets/images/michie3.png"],
+  pictures: ["/assets/images/micel.png", "/assets/images/michie3.jpg"],
   descriptions: [
     "I am creating stunning software for your business, Intermediate experienced in web design and development.",
     "Hello everyone! I am Zaheid, a frontend mobile and website developer. I am a intermediate experienced and I hope with this I can always learning something new to be able to invent digital applications that can help many people in the future. Could work independently or in a team.",
