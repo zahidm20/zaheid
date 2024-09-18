34ba2fa70638814416c38d5fd0f6a34fc08810ec
zahidm20 <117884005+zahidm20@users.noreply.github.com>
2024-09-18 08:48:38 +0700

    pus

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -5,7 +5,7 @@ export const profileData: Profile = {
   region: "Indonesia",
   CV: "/assets/pdf/cv.pdf",
   divisions: ["Fullstack and Mobile Developer", "Backend Developer", "Mobile Developer", "Software Tester"],
-  pictures: ["/assets/images/miciy.png", "/assets/images/michie3.jpg"],
+  pictures: ["/assets/images/micel.png", "/assets/images/michie3.png"],
   descriptions: [
     "I am creating stunning software for your business, Intermediate experienced in web design and development.",
     "Hello everyone! I am Zaheid, a frontend mobile and website developer. I am a intermediate experienced and I hope with this I can always learning something new to be able to invent digital applications that can help many people in the future. Could work independently or in a team.",
@@ -16,7 +16,7 @@ export const profileData: Profile = {
       title: "Years Of Experience",
     },
     {
-      number: 7,
+      number: 10,
       title: "Framework & Tech Stack Used",
     },
     {
--- src/constants/workExperience.ts
+++ src/constants/workExperience.ts
@@ -13,7 +13,7 @@ export const workExperienceList: WorkExperience[] = [
     jobDesk: 'Full Stack Web Developer',
     companyName: 'Global Digital Verse',
     years: 'May 2023 - Present',
-    jobStatus: 'Full Time',
+    jobStatus: 'Full Time', 
     jobDescription: 'Responsible for creating several features found on websites such as FindDW, Optimus, Litani and many more and responsible for creating the entire company profile website.',
     techStack: ['/assets/svg/laravel.svg', '/assets/svg/bootstrap.svg', '/assets/svg/mysql.svg', '/assets/svg/php.svg'],
   },
@@ -24,5 +24,13 @@ export const workExperienceList: WorkExperience[] = [
     jobStatus: 'Internship',
     jobDescription: 'Responsible for making several features found on the ADMTC and EDH platforms such as Search Filters and Form Validation.',
     techStack: ['/assets/svg/angular.svg', '/assets/svg/typescript.svg', '/assets/svg/graphql.svg'],
+  },
+  {
+    jobDesk: 'Back-End Developer',
+    companyName: 'Lab Asistant',
+    years: 'January 2022 - April 2022',
+    jobStatus: 'Internship',
+    jobDescription: 'Responsible for creating several API features on the website, such as CRUD, filters and many other features that have been worked on.',
+    techStack: ['/assets/svg/nest js.svg', '/assets/svg/typescript.svg', '/assets/svg/postman.svg', '/assets/svg/express.svg'],
   },
 ];
