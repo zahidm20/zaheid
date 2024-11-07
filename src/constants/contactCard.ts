0dda8d261f74ccd2bc2c8eaf6208cf6bee5419fc
zahidm20 <117884005+zahidm20@users.noreply.github.com>
2024-11-07 13:53:07 +0700

    barunih

--- src/constants/contactCard.ts
+++ src/constants/contactCard.ts
@@ -4,13 +4,13 @@ export const contactCardList: ContactCard[] = [
   {
     title: "Whatsapp",
     contactVia: "+62 82298774625",
-    contactLink: "https://wa.me/628991622164",
+    contactLink: "https://wa.me/082298774625",
     cardIcon: "/assets/svg/whatsapp.svg",
   },
   {
     title: "Gmail",
     contactVia: "zahidmaulana20@gmail.com",
-    contactLink: "https://mail.google.com/mail/u/0/?fs=1&to=muhammadsufyann09@gmail.com&su=Your%20Subject&body=Your%20Messages&tf=cm",
+    contactLink: "https://mail.google.com/mail/u/0/?fs=1&to=zahidmaulana20@gmail.com&su=Your%20Subject&body=Your%20Messages&tf=cm",
     cardIcon: "/assets/svg/gmail.svg",
   },
   {
